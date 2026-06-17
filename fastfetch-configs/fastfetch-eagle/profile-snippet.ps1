5003d29d47c99642ff6add8c0e9ade96e9095fa1
Argha Saha <84933329+arg387@users.noreply.github.com>
2026-06-17 22:22:56 +0530

    Delete fastfetch-configs/fastfetch-eagle/profile-snippet.ps1

--- fastfetch-configs/fastfetch-eagle/profile-snippet.ps1
+++
@@ -1,9 +0,0 @@
-# Eagle Fastfetch startup
-$eagleFastfetchRoot = 'D:\customization\fastfetch-configs\fastfetch-eagle'
-$eagleFastfetchConfig = Join-Path $eagleFastfetchRoot 'config.jsonc'
-
-if ((Get-Command fastfetch -ErrorAction SilentlyContinue) -and
-    (Test-Path -LiteralPath $eagleFastfetchConfig)) {
-    $env:FASTFETCH_EAGLE_ROOT = $eagleFastfetchRoot
-    fastfetch --config $eagleFastfetchConfig
-}
